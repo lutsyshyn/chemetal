4cdcf3e9bcb9a5d6a18f84270fde774183ce8f22
ub <ub@ub-VirtualBox.(none)>
2013-01-03 12:04:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chemetal::Application.config.secret_token = '71b61cd59e7a749dfc90b8af704fdcf8c376e227de910780e9b40996776a2412d6943f10fa2ee49388f7d0bdffe4d56924645d70533ebc8d32148054a383f71b'
